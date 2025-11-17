083ac883d309947868ee12096d1c754a7b551c36
LegendaryLemon <82708089+Loizz04@users.noreply.github.com>
2025-11-17 12:25:37 -0500

    Added build_Path.c and Edit_Existing_File.c; updated main.c

---
+++ PortableFileManipulator/Edit_Existing_File.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void Edit_Existing_File() {
+	printf("Enter your the directory of file: ");
+	printf("Enter file name: ");
+
+}
---
+++ PortableFileManipulator/build_Path.c
@@ -0,0 +1,25 @@
+//optional build path function 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//charecter pointer directory set to address 
+void build_Path(const char *directory, const char  *fileName, const *storeDirectory, size_t dirsize) {
+	size_t len_dir = strlen(directory);
+
+	//we first copy the directory into a directory holder
+	strncpy(storeDirectory, directory, dirsize - 1);
+	storeDirectory[dirsize - 1] = '\0';   // safety null terminator
+
+	//correct any user slash faults
+	if (len_dir > 0) {
+		char last = directory[len_dir - 1];
+		if (last != '/' && last != '\\') {
+			strncat(storeDirectory, "/", dirsize - strlen(storeDirectory) - 1);
+		}
+	}
+	//finally we append the file name 
+	strncat(storeDirectory, fileName, dirsize - strlen(storeDirectory) - 1);
+
+}
